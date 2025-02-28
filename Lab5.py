6f44b270504f7ff01821f32d897ee780ea6b6821
benazoulay <ben.azoulay27@gmail.com>
2025-02-28 14:52:01 +0000

    fimished until function 4

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,31 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ''
+    for i in range(n):
+        for j in range(n):
+            if i == 0 or i == n-1 or j == 0 or j == n-1:
+                result += '*'
+            else:
+                result += ' '
+        result += '\n'
+    return result.strip()
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ''
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            result += str(j)
+        result += '\n'
+    return result.strip()
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    return (n * (n + 1)) // 2
 
 # Example for n = 4:
 #    *
@@ -24,4 +37,9 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ''
+    for i in range(1, n + 1):
+        for j in range(1, n - i + 1):
+            result += ''
+        
+    return centered_star_pyramid(n)
